cc7a0e0f5d69b6ba4e86ee34f62296a7c8ba7bde
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-07-08 21:23:46 +0430

    modularClassNamer modified

--- src/utility/functions/modularClassNamer.js
+++ src/utility/functions/modularClassNamer.js
@@ -2,7 +2,7 @@ const modularClassNamer = (styleRef) => {
   return (classString) => {
     const classArray = classString[0].split(' ');
     const className = classArray
-      .map((classSrt) => styleRef[classSrt])
+      .map((classSrt) => (styleRef[classSrt] ? styleRef[classSrt] : classSrt))
       .toString()
       .replace(',', ' ');
     return className;
