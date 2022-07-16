cdb65e4d5ace5624dcd93aed55397c067ce876ac
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-07-16 05:00:19 +0430

    CartItem created and styled + responsiv

---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,73 @@
+import modularClassNamer from '../../utility/functions/modularClassNamer';
+import styles from './CartItem.module.css';
+import { BsFillTrashFill, BsDash, BsPlus } from 'react-icons/bs';
+import { useCartAction } from '../../providers/CartProvider/CartProvider';
+import {
+  decrementProductQuantity,
+  incrementProductQuantity,
+  removeFromCart,
+} from '../../providers/CartProvider/actionCreator';
+
+const $ = modularClassNamer(styles);
+
+const CartItem = ({ productInfo }) => {
+  const dispatch = useCartAction();
+  const { id, image, price, name, offPrice, discount, quantity } = productInfo;
+
+  return (
+    <li className={$`product__item`}>
+      {offPrice !== price && (
+        <span className={$`special-offer`}>{discount} %</span>
+      )}
+      <div className={$`image`}>
+        <img src={image} alt={name} />
+      </div>
+
+      <div className={$`info`}>
+        <p className={$`info__name`}>{name}</p>
+        {offPrice === price ? (
+          <p className={$`info__price`}>{offPrice * quantity} $ / number</p>
+        ) : (
+          <p className={$`info__price info__price--off`}>
+            off price: {offPrice} $ / number
+          </p>
+        )}{' '}
+        <p className={$`info__total-price`}>total: {offPrice * quantity} $</p>
+      </div>
+
+      <div className={$`management`}>
+        {' '}
+        {quantity > 1 ? (
+          <button
+            onClick={() => {
+              dispatch(decrementProductQuantity(productInfo));
+            }}
+            className={$`btn btn--icon management__btn management__btn--decrement`}
+          >
+            <BsDash />
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              dispatch(removeFromCart(productInfo));
+            }}
+            className={$`btn btn--icon management__btn management__btn--remove`}
+          >
+            <BsFillTrashFill />
+          </button>
+        )}
+        <p className={$`quantity`}>{quantity}</p>
+        <button
+          onClick={() => {
+            dispatch(incrementProductQuantity(productInfo));
+          }}
+          className={$`btn btn--icon management__btn management__btn--increment`}
+        >
+          <BsPlus />
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
