fde4a8dc99d5ea5994d1520adfe59f9912cf70b9
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-07-21 02:28:50 +0430

    Login and signup Pages added to routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import CartPage from './pages/CartPage/CartPage';
 import HomePage from './pages/HomePage/HomePage';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 
 const routes = [
-  { path: '/cart', element: <CartPage />, key: 1 },
-  { path: '/', element: <HomePage />, key: 2 },
+  { path: '/', element: <HomePage />, key: 1 },
+  { path: '/cart', element: <CartPage />, key: 2 },
+  { path: '/login', element: <Login />, key: 3 },
+  { path: '/signup', element: <SignUp />, key: 4 },
 ];
 export default routes;
