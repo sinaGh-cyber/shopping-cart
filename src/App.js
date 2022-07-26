52b396b9f70cd2eb17aba4a6323df85e73f71b7f
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-07-26 12:42:12 +0430

    App provided by AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Layout from './Layout/Layout';
-import CartProvider from './providers/CartProvider/CartProvider';
+import CartProvider from './providers/Cart/CartProvider';
 import routes from './routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AuthProvider from './providers/Auth/AuthProvider';
 
 function App() {
   return (
     <div className="App">
       {' '}
       <Router>
-        <CartProvider>
-          <ToastContainer theme="colored" style={{ fontSize: '2rem' }} />
-          <Routes>
-            {routes.map((route) => {
-              return <Route {...route} />;
-            })}
-          </Routes>
-        </CartProvider>
+        <AuthProvider>
+          <CartProvider>
+            <ToastContainer theme="colored" style={{ fontSize: '2rem' }} />
+            <Routes>
+              {routes.map((route) => {
+                return <Route {...route} />;
+              })}
+            </Routes>
+          </CartProvider>
+        </AuthProvider>
       </Router>
     </div>
   );
