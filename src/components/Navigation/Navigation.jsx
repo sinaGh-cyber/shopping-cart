728df637f3d955969e5cf70bf2d9f0b5b956e91e
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-07-26 12:46:20 +0430

    Navigation component modified

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,17 @@
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../providers/Auth/AuthProvider';
+import { BsCart4 } from 'react-icons/bs';
+import { AiTwotoneAppstore } from 'react-icons/ai';
+
+import { useCart } from '../../providers/Cart/CartProvider';
 import modularClassNamer from '../../utility/functions/modularClassNamer';
 import styles from './Navigation.module.css';
 
 const $ = modularClassNamer(styles);
 
 const Navigation = () => {
+  const auth = useAuth();
+  const { cart } = useCart();
   return (
     <nav className={$`nav`}>
       <ul className={$`nav__list`}>
@@ -16,7 +23,10 @@ const Navigation = () => {
               isActive ? $`nav__item--active` : undefined
             }
           >
-            <button>Cart</button>
+            <button className={$`cart-navigation-btn`}>
+              <BsCart4 />
+              <span className={$`cart-counter-badge`}>{cart.length}</span>
+            </button>
           </NavLink>{' '}
         </li>
         <li className={$`nav__item`}>
@@ -27,8 +37,13 @@ const Navigation = () => {
               isActive ? $`nav__item--active` : undefined
             }
           >
-            <button>Home</button>
+            <button>
+              <AiTwotoneAppstore />
+            </button>
           </NavLink>
+        </li>
+        <li className={$`nav__item`}>
+          {auth.notHasAuthData ? 'Enter / Register' : auth.name}
         </li>
       </ul>
     </nav>
